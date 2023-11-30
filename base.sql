9087111e6dfead543aaa8e927b4a08d799d96bc0
Miandritiana <miandrititana21@gmail.com>
2023-11-30 10:49:57 +0300

    suite base

--- base.sql
+++ base.sql
@@ -28,3 +28,27 @@ create table defense (
     note int,
     idType varchar(10) references type(idType)
 );
+
+create table attaque (
+    idAttaque varchar(10) primary key,
+    idEquipe varchar(10) references equipe(idEquipe),
+    tirs_pm int,
+    tirs_CA_pm int,
+    dribbles_pm int,
+    fautes_subies_pm int,
+    note int,
+    idType varchar(10) references type(idType)
+);
+
+create table generale (
+    idGenerale varchar(10) primary key,
+    idEquipe varchar(10) references equipe(idEquipe),
+    buts int,
+    tirs_pm int,
+    discipline int,
+    possession int,
+    passesReussies int,
+    aeriensGagnes int,
+    note int,
+    idType varchar(10) references type(idType)
+);
